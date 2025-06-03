55480a0b3b0d0c1093cfc52ae7c4674177021c94
Monish N <monish7549@gmail.com>
2025-06-04 01:35:20 +0530

    Add files via upload
    Added calculator.py and README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Basic calculator using python
+
+Hello, am Monish BCA graduate, currently in MCA specializing in Generative AI.
+This is my first python based mini project - a basic calculator that performs arithmetic operations using terminal input
+
+# Features :
+- add,subtract,divide,multiply
+- clean output with exception handling, positive,negative & decimal numbers, dividing by 0 and all
+
+I practiced using python functions, loops & user input; handled 'value error' and 'keyboard interrupt'
+I honestly didn't do everything myself, took some reference via chatGPT if any errors such and confirm those logics
+
+I may be fresher for now in these, but i will work hard for it, with more hands on practice & work experience i may work smartly and grow more concisely in my life 
---
+++ basic_calculator.py
@@ -0,0 +1,67 @@
+# Basic calculator with simple math arithmetic operations
+# Written for practice with Python as well user input concept, mini project
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+def calculator():
+    print("\n--- Simple Calculator ---")    
+    print("1. Add")
+    print("2. Subtract") 
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
+    
+    while True:
+        try:
+            choice = input("\nSelect operation (1-5): ")
+            
+            if choice == '5':
+                print("See you buddy")
+                break
+                
+            if choice not in ['1', '2', '3', '4']:
+                print("Error... Try again.")
+                continue
+            
+            # Get input i.e, number from user
+
+            num1 = float(input("First number: "))
+            num2 = float(input("Second number: "))
+            
+            # Do yo8r calculation as such based on choice
+
+            if choice == '1':
+                result = add(num1, num2)
+                op = '+'
+            elif choice == '2':
+                result = subtract(num1, num2)
+                op = '-'
+            elif choice == '3':
+                result = multiply(num1, num2)
+                op = '*'
+            elif choice == '4':
+                result = divide(num1, num2)
+                op = '/'
+            
+            print(f"Result: {num1} {op} {num2} = {result}")
+            
+        except ValueError:
+            print("Invalid input, Enter numbers only :(")
+        except KeyboardInterrupt:
+            print("\nNice interacting with you, Goodbye:)")
+            break
+
+if __name__ == "__main__":
+    calculator()
